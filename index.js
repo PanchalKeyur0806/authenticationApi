8eb47757926305d027ca3836f028a593116180c3
PanchalKeyur0806 <panchalkeyur694@outlook.com>
2025-07-12 18:28:58 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,25 @@
+require("dotenv").config();
+
+const express = require("express");
+const mongoose = require("mongoose");
+const cookieParser = require("cookie-parser");
+
+const app = express();
+
+// connecting to database
+mongoose
+  .connect(process.env.MONGO_URL)
+  .then(() => {
+    console.log("Database connected successfully");
+  })
+  .catch((error) => {
+    console.log("ERROR WHILE CONNECTING TO DB ", error);
+  });
+
+// some middlewares
+app.use(express.json());
+app.use(cookieParser());
+
+app.listen(process.env.PORT, () => {
+  console.log("server is running on port ", process.env.PORT);
+});
